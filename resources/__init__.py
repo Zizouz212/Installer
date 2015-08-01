2cf5dcbe3b3fe2999459c54fcf2b421b24ed86de
Zizouz212 <Zizouz212@users.noreply.github.com>
2015-08-01 23:09:27 +0200

    Update __init__.py

--- resources/__init__.py
+++ resources/__init__.py
@@ -2,5 +2,22 @@
 
 # __init__.py
 
-# import stuff, create variables from files, store all the data here.
+# import stuff, create variables from files, store all the data here. Get platform details - Platform checks in next release?
 
+__doc__ = """
+Ease of use to provide all resources and data necessary for operation.
+"""
+
+import sys
+import platform
+import os
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+with open(os.path.join(base_path, "license.txt") as f:
+    license = f.read()
+
+with open(os.path.join(base_path, "eula.txt") as f:
+    eula = f.read()
+    
